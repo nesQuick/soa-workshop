3f3f44c024dfa01ef96c079d8e078ee83ca61b26
Thorben Schröder <stillepost@gmail.com>
2014-03-12 01:13:26 +0100

    Adds challenge 7

---
+++ box/challenges/07/README.md
@@ -0,0 +1,7 @@
+# Challenge 07
+
+Turn the app from [challenge 06](/walski/soa-workshop/tree/master/box/challenges/06) into a gem called ``hello_world_app``. Make sure to build that gem into the ``hello_world_app`` sub-directory in this challenge's folder.
+
+# Setup
+
+Once your gem is ready run ``bundle install`` before you can run the specs to test your gem.
---
+++ box/challenges/07/spec/hello_world_spec.rb
@@ -0,0 +1,61 @@
+require_relative '../../shared/spec_helper'
+require 'hello_world_app'
+
+describe HelloWorldApp do
+  describe "authorized" do
+    before do
+      authorize 'last', 'unicorn'
+    end
+
+    describe "/hello" do
+      before do
+        get '/hello'
+      end
+
+      it "returns status 200" do
+        expect(response.status).to be == 200
+      end
+
+      it "has a 'Hello' body" do
+        expect(response.body).to be == 'Hello'
+      end
+    end
+
+    describe "/world" do
+      before do
+        get '/world'
+      end
+
+      it "returns status 200" do
+        expect(response.status).to be == 200
+      end
+
+      it "has a 'World' body" do
+        expect(response.body).to be == 'World'
+      end
+    end
+
+    describe "random url" do
+      before do
+        get "/something-else"
+      end
+
+      it "returns status 404" do
+        expect(response.status).to be == 404
+      end
+
+      it "has a 'Not Found' body" do
+        expect(response.body).to be == 'Not Found'
+      end
+    end
+  end
+
+  describe "unauthorized" do
+    %w{/hello /world}.each do |path|
+      it "returns 401 for #{path}" do
+        get path
+        expect(response.status).to be == 401
+      end
+    end
+  end
+end
